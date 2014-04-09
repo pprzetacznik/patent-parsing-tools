d3d8c75dfc23c456b91eaf03fa39e0276c07bf2a
Piotrek <piotr.przetacznik@gmail.com>
2014-04-09 16:15:08 +0200

    adding gitignore, main to both files, and some enter conditions

--- extractor.py
+++ extractor.py
@@ -1,7 +1,26 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import os
 import xml.etree.ElementTree as ET
+import sys
 
-inputfile = 'c:\\patenty\\ipg131224\\concated\\USD0695987-20131224.XML'
 
-tree = ET.parse(inputfile)
-root = tree.getroot()
-print "Document ID:", root.findall('.//publication-reference//document-id//doc-number')[0].text
+def main(inputfile):
+    tree = ET.parse(inputfile)
+    root = tree.getroot()
+    print "Document ID:", root.findall('.//publication-reference//document-id//doc-number')[0].text
+    print "Invention Title:", root.findall('.//us-bibliographic-data-grant//invention-title')[0].text
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        inputfile = sys.argv[1]
+    else:
+        inputfile = 'c:\\patenty\\ipg131224\\concated\\US08613112-20131224.XML'
+
+    if os.path.isfile(inputfile):
+        main(inputfile)
+    else:
+        print "File: " + inputfile + " doesn't exist"
--- splitter.py
+++ splitter.py
@@ -1,28 +1,52 @@
-import os, re
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
-input = "c:\\patenty\\ipg131224\\concated.xml"
-dir = "c:\\patenty\\ipg131224\\concated"
+import os, re
+import sys
+
 
 def get_headers_and_filename(file):
     headers = []
     for i in range(3):
         line = file.readline()
         headers.append(line)
-    print headers[2]
-    return headers, re.match(r'.*file=.([^ ]*)\".*', headers[2]).group(1)
+    node = re.match(r'.*file=.([^ ]*)\".*', headers[2])
+    if node:
+        return headers, re.match(r'.*file=.([^ ]*)\".*', headers[2]).group(1)
+    return None, None
 
-def split_file(input_file):
+
+def split_file(input_file, dir):
     fread = open(input_file)
     os.makedirs(dir)
     eof = os.fstat(fread.fileno()).st_size
     while fread.tell() < eof:
         headers, filename = get_headers_and_filename(fread)
+        if filename is None:
+            break
+
         print filename
+        print headers[2]
+        print "============================================="
+
         fwrite = open(os.path.join(dir, filename), 'w')
         for line in headers: fwrite.write(line)
         while True:
             line = fread.readline()
             fwrite.write(line)
-            if line.startswith('</us-patent-grant'): break
+            if line.startswith('</us-patent-grant'):
+                break
 
-split_file(input)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        input = sys.argv[1]
+        dir = sys.argv[2]
+    else:
+        input = "c:\\patenty\\ipg131224\\ipg131224.xml"
+        dir = "c:\\patenty\\ipg131224\\concated"
+
+    if os.path.isfile(input):
+        split_file(input, dir)
+    else:
+        print "File: " + input + " doesn't exist"
