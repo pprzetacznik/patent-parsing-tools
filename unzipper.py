97917c4fd973e25ca4bee33b839c34ed23926a6c
michal <michaldul@outlook.com>
2014-05-31 12:30:35 +0200

    improved unzipper

--- unzipper.py
+++ unzipper.py
@@ -4,7 +4,7 @@
 from zipfile import is_zipfile, ZipFile
 from sys import argv
 from os import listdir
-from os.path import join
+from os.path import join, isfile
 
 class Unzipper():
     def __init__(self, directory):
@@ -13,8 +13,17 @@ class Unzipper():
     def unzip_all(self):
         zip_files = [ join(self.directory, f)
                       for f in listdir(self.directory) if is_zipfile(join(self.directory, f)) ]
-        for zip in zip_files:
-            ZipFile(zip).extractall(self.directory)
+        for zip_name in zip_files:
+            zip = ZipFile(zip_name)
+            if self.should_be_unzipped(zip):
+                print "unzip", zip_name
+                zip.extractall(self.directory)
+
+    def should_be_unzipped(self, zip):
+        for file in zip.namelist():
+            if not isfile(join(self.directory, file)):
+                return True
+        return False
 
 if __name__ == '__main__':
     if len(argv) == 2:
