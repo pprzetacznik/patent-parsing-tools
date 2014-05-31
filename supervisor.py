95d4f8e778b16a3e9b8286a9d461f0b92a768e3e
Piotrek <piotr.przetacznik@gmail.com>
2014-05-31 16:57:27 +0200

    added logger to supervisor

--- supervisor.py
+++ supervisor.py
@@ -9,11 +9,13 @@ from downloader import Downloader
 from unzipper import Unzipper
 import splitter
 from extractor import Extractor
+from logger import Logger
 
 
 class Supervisor():
     def __init__(self, working_dir):
         self.working_dir = working_dir
+        self.logger = Logger().getLogger("Supervisor")
 
     def begin(self, begin_year, end_year):
         self.download_archives(begin_year, end_year)
@@ -22,28 +24,32 @@ class Supervisor():
         self.extract_data()
 
     def download_archives(self, begin_year, end_year):
+        self.logger.info("downloading archives")
         print 'downloading archives'
         downloader = Downloader('https://www.google.com/googlebooks/uspto-patents-grants-text.html')
         downloader.download_archives(self.working_dir, begin_year, end_year)
 
     def unzip_archives(self):
-        print 'unzipping archives'
+        self.logger.info("unzipping archives")
         unzipper = Unzipper(self.working_dir)
         unzipper.unzip_all()
 
     def split_archives(self):
-        print 'splitting files'
+        self.logger.info("splitting files")
         xmls = get_files(self.working_dir, ".xml")
         for file in xmls:
             splitter.split_file(file, join(self.working_dir, "patents"))
 
     def extract_data(self):
-        print 'extracting data'
+        self.logger.info("extracting data")
         extractor = Extractor("extractor_configuration.json", self.working_dir)
         patents = get_files(join(self.working_dir, "patents"), ".XML")
         for patent in patents:
-            print "extracting", patent
-            extractor.parse_and_save_to_database(patent)
+            self.logger.info("extracting " + patent)
+            try:
+                extractor.parse_and_save_to_database(patent)
+            except Exception as e:
+                self.logger.error(e.message, e.args)
 
 
 def get_files(directory, type):
