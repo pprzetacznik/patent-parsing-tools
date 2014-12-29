be0a38eaa07f942c489e8c33b4d1f17a6a685b64
Adun91 <eladen@o2.pl>
2014-12-29 19:48:43 +0100

    bug

--- read_pickle.py
+++ read_pickle.py
@@ -1,5 +1,6 @@
 import sys
 import cPickle
+from patent import Patent
 
 __author__ = 'vreal'
 
@@ -7,8 +8,16 @@ __author__ = 'vreal'
 if __name__ == '__main__':
     pikle_name = sys.argv[1]
     a = cPickle.load(open(pikle_name, "rb"))
-    if len(sys.argv) == 2:
-        print str(a)
-    elif len(sys.argv) == 3:
-        string_length = int(sys.argv[2])
-        print str(a)[0:string_length]
+
+    if isinstance(a, list):
+        p = a[0]
+        if isinstance(p, Patent):
+            for patent in a:
+                print patent.title
+                print patent.classification
+    else :
+        if len(sys.argv) == 2:
+            print str(a)
+        elif len(sys.argv) == 3:
+            string_length = int(sys.argv[2])
+            print str(a)[0:string_length]
