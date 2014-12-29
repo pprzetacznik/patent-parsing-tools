31c6ccd4c581d2177c4107a284c06db5366336d8
Adun91 <eladen@o2.pl>
2014-12-29 19:37:31 +0100

    bug

--- read_pickle.py
+++ read_pickle.py
@@ -5,7 +5,7 @@ __author__ = 'vreal'
 
 
 if __name__ == '__main__':
-    pikle_name = "/home/vreal/deepLearning/test_set/docs/xml_tuple_1"
+    pikle_name = sys.argv[1]
     patent_list = cPickle.load(open(pikle_name, "rb"))
     if len(sys.argv) == 2:
         for patent in patent_list:
